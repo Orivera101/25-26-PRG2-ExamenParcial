392e639e2df694325111a22c8cd74cce081709ba
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-06 14:45:02 +0000

    Implementacion de constructores de la clase Cuenta

--- entregas/riveraOscar/src/Cuenta.java
+++ entregas/riveraOscar/src/Cuenta.java
@@ -1,7 +1,19 @@
 class Cuenta{
-    public Cuenta(String IBAN, double saldo, String tipo){}
-    public Cuenta(String IBAN){}
-    public Cuenta(){}
+    private String IBAN;
+    private double saldo;
+    private String tipo;
+
+    public Cuenta(String IBAN, double saldo, String tipo){
+        this.IBAN = IBAN;
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
+    public Cuenta(String IBAN){
+        this(IBAN, 0.0, "Estandar")
+    }
+    public Cuenta(){
+        this("ES91 0000 0000 0000 0000", 0.0, "Estandar")
+    }
 
     public void modificarCuenta(String IBAN, double saldo, String tipo){}
     public void mostrarCuenta(){}
