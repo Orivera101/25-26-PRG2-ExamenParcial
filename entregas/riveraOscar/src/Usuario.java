00f1abbc839555b1b57c0a0e193119f2ed36f094
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-06 14:41:45 +0000

    Implementacion de metodos de la clase Usuario

--- entregas/riveraOscar/src/Usuario.java
+++ entregas/riveraOscar/src/Usuario.java
@@ -20,10 +20,29 @@ class Usuario {
         this("DNI00000000");
     }
 
-    public void asignarCuenta(Cuenta cuenta){}
-    public Usuario clonarUsuario(){}
-    public void mostrarUsuario(){}
-    public void modificarUsuario(String nombre, String DNI, String direccion){}
-    public Cuenta[] mostrarCuentasUsuario(){}
-    public Cuenta mostrarUnaCuenta(int numeroCuenta){}
+    public void asignarCuenta(Cuenta cuenta){
+        for (int i = 0; i < this.cuentas.length; i++) {
+            if (this.cuentas[i] == null) {
+                this.cuentas[i] = cuenta;
+                break;
+            }
+        }
+    }
+    public Usuario clonarUsuario(){
+        return new Usuario(this.nombre, this.DNI, this.direccion, this.cuentas);
+    }
+    public void mostrarUsuario(){
+        System.out.println("DNI: " + DNI + "\nNombre: " + nombre + "\nDirección: " + direccion);
+    }
+    public void modificarUsuario(String nombre, String DNI, String direccion){
+        this.nombre = nombre;
+        this.DNI = DNI;
+        this.direccion = direccion;
+    }
+    public Cuenta[] mostrarCuentasUsuario(){
+        return this.cuentas;
+    }
+    public Cuenta mostrarUnaCuenta(int numeroCuenta){
+        return this.cuentas[numeroCuenta];
+    }
 }
