eaa267fba557d6e6176e6e8d88205749a30955cf
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-06 17:34:22 +0000

    Implementacion de los metodos de la clase Cuenta

--- entregas/riveraOscar/src/Cuenta.java
+++ entregas/riveraOscar/src/Cuenta.java
@@ -2,25 +2,58 @@ class Cuenta{
     private String IBAN;
     private double saldo;
     private String tipo;
+    private String historial = "";
 
     public Cuenta(String IBAN, double saldo, String tipo){
         this.IBAN = IBAN;
         this.saldo = saldo;
         this.tipo = tipo;
+        this.historial = this.historial + "Cuenta creada con saldo: " + saldo + "€\n";
     }
     public Cuenta(String IBAN){
-        this(IBAN, 0.0, "Estandar")
+        this(IBAN, 0.0, "Estandar");
     }
     public Cuenta(){
-        this("ES91 0000 0000 0000 0000", 0.0, "Estandar")
+        this("ES91 0000 0000 0000 0000", 0.0, "Estandar");
     }
 
-    public void modificarCuenta(String IBAN, double saldo, String tipo){}
-    public void mostrarCuenta(){}
-    public Cuenta clonarCuenta(){}
-    public void ingresar(double monto){}
-    public void retirar(double monto){}
-    public void transferir(double monto, Cuenta cuenta){}
-    public double consultarSaldo(){}
-    public void consultarMovimientos(){}
+    public void modificarCuenta(String IBAN, double saldo, String tipo){
+        this.IBAN = IBAN;
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
+    public void mostrarCuenta(){
+        System.out.println("IBAN: " + IBAN + "\nSaldo: " + saldo + "\nTipo: " + tipo);
+    }
+    public Cuenta clonarCuenta(){
+        return new Cuenta(this.IBAN, this.saldo, this.tipo);
+    }
+    public void ingresar(double monto){
+        saldo = saldo + monto;
+        this.historial = this.historial + "Ingreso: +" + monto + "€\n";
+    }
+    public void retirar(double monto){
+        if (saldo >= monto) {
+            saldo = saldo - monto;
+            this.historial = this.historial + "Retiro: -" + monto + "€\n";
+        }
+        else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+    public void transferir(double monto, Cuenta cuenta){
+        if (saldo >= monto) {
+            retirar(monto);
+            cuenta.ingresar(monto);
+            this.historial += "Transferencia enviada: -" + monto + "€ a " + cuenta.IBAN + "\n";
+        }
+    }
+    public double consultarSaldo(){
+        System.out.println("La cuenta " + IBAN + " cuenta con un saldo de " + saldo + "€.");
+        return saldo;
+    }
+    public void consultarMovimientos(){
+        System.out.println("--- Movimientos de la cuenta " + IBAN + " ---");
+        System.out.println(historial);
+    }
 }
