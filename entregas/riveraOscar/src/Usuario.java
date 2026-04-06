ab3da7def0309defbae6ded170f8b461f8b9dc17
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-06 14:29:13 +0000

    Constructores de Usuario implementados

--- entregas/riveraOscar/src/Usuario.java
+++ entregas/riveraOscar/src/Usuario.java
@@ -4,10 +4,21 @@ class Usuario {
     private String direccion;
     private Cuenta[] cuentas;
 
-    public Usuario(String nombre, String DNI, String direccion){}
-    public Usuario(String nombre, String DNI, String direccion, Cuenta[] cuentas){}
-    public Usuario(String DNI){}
-    public Usuario(){}
+    public Usuario(String nombre, String DNI, String direccion){
+        this(nombre, DNI, direccion, new Cuenta[10]);
+    }
+    public Usuario(String nombre, String DNI, String direccion, Cuenta[] cuentas){
+        this.nombre = nombre;
+        this.DNI = DNI;
+        this.direccion = direccion;
+        this.cuentas = cuentas;
+    }
+    public Usuario(String DNI){
+        this("Sin nombre", DNI, "Sin direccion");
+    }
+    public Usuario(){
+        this("DNI00000000");
+    }
 
     public void asignarCuenta(Cuenta cuenta){}
     public Usuario clonarUsuario(){}
